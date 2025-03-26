8c1b2be6c7cc8583d037a6d0e176a667197d8b2f
Adir Noyman <ofahol2007@hotmail.com>
2025-03-26 21:17:21 +0200

    .

--- AdirSkiStore/client/src/app/layout/App.tsx
+++ AdirSkiStore/client/src/app/layout/App.tsx
@@ -1,10 +1,25 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../models/product';
 import Catalog from '../../features/catalog/Catalog';
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import NavBar from './NavBar';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const theme = createTheme({
+    palette: {
+      mode: isDarkMode ? 'dark' : 'light',
+      background: {
+        default: isDarkMode ? '#121212' : '#eaeaea',
+    }
+  }
+  });
+
+  const handleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   useEffect(() => {
     fetch('https://localhost:5001/api/products')
@@ -15,17 +30,15 @@ function App() {
   console.log(products);
 
   return (
-    <Container maxWidth="xl">
-      <Box display='flex' justifyContent='center' gap={3} marginY={3}>
-      <Typography variant='h4'>My Store 🤓</Typography>
-      <Button variant="contained" color="primary">
-        Add product
-      </Button>
-      </Box>
-     
-      <Catalog products={products}/>
-   
-    </Container>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <NavBar changeTheme={handleDarkMode} isDarkMode={isDarkMode}/>
+      <Box sx={{minHeight: '100vh', bgcolor: 'background.default'}}>
+      <Container maxWidth="xl" sx={{ mt: 13 }}>
+        <Catalog products={products} />
+      </Container>
+      </Box>      
+    </ThemeProvider>
   );
 }
 
