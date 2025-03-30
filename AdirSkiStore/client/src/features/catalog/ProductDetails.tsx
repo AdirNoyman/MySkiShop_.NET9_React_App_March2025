fb34df61112315da952bb7ea3e4e71aff748e375
Adir Noyman <ofahol2007@hotmail.com>
2025-03-30 21:36:39 +0300

    .

--- AdirSkiStore/client/src/features/catalog/ProductDetails.tsx
+++ AdirSkiStore/client/src/features/catalog/ProductDetails.tsx
@@ -1,33 +1,97 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Product } from "../../app/models/product";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Product } from '../../app/models/product';
+import {
+  Button,
+  Divider,
+  Grid2,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 const ProductDetails = () => {
-
   // Get the product ID from the URL
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
-
-    fetch(`https://localhost:5001/api/products/${id}`).then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Network response was not ok 😫');
-    }
-    ).then(data => {
-      setProduct(data);
-    }
-    ).catch(error => {
-      console.error('Oops! - problem fetching the product 😫', error);
-    })
-
+    fetch(`https://localhost:5001/api/products/${id}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok 😫');
+      })
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((error) => {
+        console.error('Oops! - problem fetching the product 😫', error);
+      });
   }, [id]);
 
-  return (
-    <div>ProductDetails: {product?.name}</div>
-  )
-}
+  if (!product) return <h3>Loading product...</h3>;
 
-export default ProductDetails
+  const productDetails = [
+    { label: 'Name', value: product.name },
+    { label: 'Description', value: product.description },
+    { label: 'Type', value: product.type },
+    { label: 'Brand', value: product.brand },    
+    { label: 'Quantity in stock', value: product.quantityInStock },
+  ]
+
+  return (
+    <Grid2 container spacing={6} maxWidth='lg' sx={{ mx: 'auto' }}>
+      <Grid2 size={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: '100%', borderRadius: '10px' }}
+        />
+      </Grid2>
+      <Grid2 size={6}>
+        <Typography variant='h3'>{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant='h4' color='secondary'>
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table sx={{ '& td': { fontSize: '1rem' } }}>
+            <TableBody>
+              {productDetails.map((detail, index) => (
+                <TableRow key={index}>
+                 <TableCell sx={{fontWeight: 'bold'}}>{detail.label}</TableCell>
+                 <TableCell>{detail.value}</TableCell>
+                </TableRow>
+              ))}
+             
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Grid2 container spacing={2} marginTop={3}>
+          <Grid2 size={6}>
+            <TextField
+              variant='outlined'
+              type='number'
+              label='Quantity in basket'
+              fullWidth
+              defaultValue={1}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <Button color='primary' size='large' variant='contained' sx={{ height: '55px' }}>
+              Add to Basket
+            </Button>
+          </Grid2>
+        </Grid2>
+      </Grid2>
+    </Grid2>
+  );
+};
+
+export default ProductDetails;
